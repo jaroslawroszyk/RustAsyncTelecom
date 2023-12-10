efba7195efd644ed16a7e7fcfaf700fd6d5dfb6b
Jaroslaw <roszyk.jarek@gmail.com>
2023-12-10 22:17:20 +0100

    Zmq sync/async playground

---
+++ asyncZmqExample/client/src/main.rs
@@ -0,0 +1,24 @@
+use async_zmq::{Context, Result, zmq};
+use tokio::time::Duration;
+
+async fn run_client() -> Result<()> {
+    let context = Context::new();
+    let socket = context.socket(zmq::SUB)?;
+    socket.connect("tcp://127.0.0.1:5555")?;
+    socket.set_subscribe(b"")?;
+
+    loop {
+        if let Ok(message) = socket.recv_msg(0) {
+            let message_str = message.as_str().unwrap_or_default();
+            println!("Received: {}", message_str);
+        }
+
+        tokio::time::sleep(Duration::from_secs(1)).await;
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    run_client().await?;
+    Ok(())
+}
