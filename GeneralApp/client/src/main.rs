af8f3987560dcb06b8e2787d1fa962f088322309
Jaroslaw <roszyk.jarek@gmail.com>
2024-01-13 01:20:44 +0100

    state machines introduce

--- GeneralApp/client/src/main.rs
+++ GeneralApp/client/src/main.rs
@@ -1,10 +1,22 @@
 use anyhow::Result;
-use client::run_client;
+use async_zmq::{zmq, Context};
+use client::fsm::machines::run_state_machine;
 
-mod client;
+//TODO: wysylamy heartbet i nie czekamy na response - dodac ze jak response nie przyjdzie nie idzie dalej!
+pub async fn run_client() -> Result<()> {
+    let context = Context::new();
+    let socket = context.socket(zmq::DEALER)?;
+
+    run_state_machine(&socket).await?;
+
+    Ok(())
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
     run_client().await?;
     Ok(())
 }
+/*
+TODO: jezeli nie przyjdzie resposne na heartbeat to zamknij gniazdo! (TIMEOUT?)
+*/
