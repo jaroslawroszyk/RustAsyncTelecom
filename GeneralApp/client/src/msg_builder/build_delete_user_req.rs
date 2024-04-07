e6d57dc3e3a3ab8c63c49e4361e843201257aee1
Jaroslaw Roszyk <roszyk.jarek@gmail.com>
2024-04-07 22:31:58 +0200

    tests: add first test

--- GeneralApp/client/src/msg_builder/build_delete_user_req.rs
+++ GeneralApp/client/src/msg_builder/build_delete_user_req.rs
@@ -8,3 +8,23 @@ pub fn build_delete_user_req(id: u32) -> Envelope {
 
     msg
 }
+
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_build_delete_user_req() {
+        let id: u32 = 123;
+
+        let envelope = build_delete_user_req(id);
+
+        assert!(envelope.has_delete_user_request());
+
+        let delete_user_request = envelope.delete_user_request();
+
+        assert_eq!(delete_user_request.user_id, id);
+    }
+}
